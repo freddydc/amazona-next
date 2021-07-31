9e15f529817c20d4760f3a902fcaa2d002194c21
freddydc <freddydiaz.liv@gmail.com>
2021-07-31 11:44:00 -0500

    Fix: SSR Material UI Styles

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
   return <Component {...pageProps} />;
 };
 
