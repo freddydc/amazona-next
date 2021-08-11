27536842519230c9df67e7671058ba09d0576df9
freddydc <freddydiaz.liv@gmail.com>
2021-08-11 10:54:44 -0500

    Update Remove Items In Cart

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import {
   Button,
   Card,
@@ -9,12 +10,29 @@ import {
   Typography,
 } from '@material-ui/core';
 import NextLink from 'next/link';
+import { useRouter } from 'next/router';
 import Layout from '../components/Layout/Layout';
 import Product from '@models/Product/Product';
+import { StoreContext } from '@utils/store/Store';
 import db from '@database';
+import axios from 'axios';
 
 const Home = (props) => {
   const { products } = props;
+  const router = useRouter();
+  const { state, dispatch } = useContext(StoreContext);
+
+  const addToCartHandler = async (product) => {
+    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    router.push('/cart');
+  };
 
   return (
     <Layout>
@@ -38,7 +56,11 @@ const Home = (props) => {
                 </NextLink>
                 <CardActions>
                   <Typography>${product.price}</Typography>
-                  <Button size="small" color="primary">
+                  <Button
+                    size="small"
+                    color="primary"
+                    onClick={() => addToCartHandler(product)}
+                  >
                     Add To Cart
                   </Button>
                 </CardActions>
--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -20,7 +20,7 @@ import { useRouter } from 'next/router';
 
 const ProductScreen = (props) => {
   const router = useRouter();
-  const { dispatch } = useContext(StoreContext);
+  const { state, dispatch } = useContext(StoreContext);
   const classes = useStyles();
   const { product } = props;
 
@@ -29,12 +29,14 @@ const ProductScreen = (props) => {
   }
 
   const addToCartHandler = async () => {
+    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
     const { data } = await axios.get(`/api/products/${product._id}`);
-    if (data.countInStock <= 0) {
+    if (data.countInStock < quantity) {
       window.alert('Sorry. Product is out of stock');
       return;
     }
-    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: 1 } });
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
     router.push('/cart');
   };
 
