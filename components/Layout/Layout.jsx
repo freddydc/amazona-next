245e8bc5bba76fc1813cea0263ab60ffa118216c
freddydc <freddydiaz.liv@gmail.com>
2021-08-26 19:05:48 -0500

    Create Sidebar

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,14 +1,20 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Head from 'next/head';
 import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 import {
   AppBar,
   Button,
+  Box,
+  Drawer,
+  Divider,
   Toolbar,
   Typography,
   Container,
   Link,
+  List,
+  ListItem,
+  ListItemText,
   createTheme,
   ThemeProvider,
   CssBaseline,
@@ -16,17 +22,47 @@ import {
   Badge,
   Menu,
   MenuItem,
+  IconButton,
 } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import CancelIcon from '@material-ui/icons/Cancel';
 import useStyles from './styles/styles';
 import { StoreContext } from '../../utils/store/Store';
+import { useSnackbar } from 'notistack';
+import { getError } from '@utils/error/error';
 import Cookies from 'js-cookie';
+import axios from 'axios';
 
 const Layout = ({ title, description, children }) => {
   const router = useRouter();
   const [anchorEl, setAnchorEl] = useState(null);
+  const { enqueueSnackbar } = useSnackbar();
   const { state, dispatch } = useContext(StoreContext);
   const { darkMode, cart, userInfo } = state;
   const classes = useStyles();
+
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  const sidebarOpenHandler = () => {
+    setSidebarVisible(true);
+  };
+  const sidebarCloseHandler = () => {
+    setSidebarVisible(false);
+  };
+
+  const fetchCategories = async () => {
+    try {
+      const { data } = await axios.get('/api/products/categories');
+      setCategories(data);
+    } catch (err) {
+      enqueueSnackbar(getError(err), { variant: 'error' });
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
   const theme = createTheme({
     typography: {
@@ -85,12 +121,60 @@ const Layout = ({ title, description, children }) => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <AppBar className={classes.navbar} position="static">
-          <Toolbar>
-            <NextLink href="/" passHref>
-              <Link>
-                <Typography className={classes.brand}>Magazine</Typography>
-              </Link>
-            </NextLink>
+          <Toolbar className={classes.toolbar}>
+            <Box display="flex" alignItems="center">
+              <IconButton
+                edge="start"
+                aria-label="open drawer"
+                onClick={sidebarOpenHandler}
+              >
+                <MenuIcon className={classes.navbarButton} />
+              </IconButton>
+              <NextLink href="/" passHref>
+                <Link>
+                  <Typography className={classes.brand}>magazine</Typography>
+                </Link>
+              </NextLink>
+            </Box>
+            <Drawer
+              anchor="left"
+              open={sidebarVisible}
+              onClose={sidebarCloseHandler}
+            >
+              <List>
+                <ListItem>
+                  <Box
+                    display="flex"
+                    justifyContent="space-between"
+                    alignItems="center"
+                  >
+                    <Typography>Shopping by category</Typography>
+                    <IconButton
+                      aria-label="close"
+                      onClick={sidebarCloseHandler}
+                    >
+                      <CancelIcon />
+                    </IconButton>
+                  </Box>
+                </ListItem>
+                <Divider light />
+                {categories.map((category) => (
+                  <NextLink
+                    key={category}
+                    href={`/search?category=${category}`}
+                    passHref
+                  >
+                    <ListItem
+                      button
+                      component="a"
+                      onClick={sidebarCloseHandler}
+                    >
+                      <ListItemText primary={category} />
+                    </ListItem>
+                  </NextLink>
+                ))}
+              </List>
+            </Drawer>
             <div className={classes.grow}></div>
             <div>
               <Switch
@@ -99,16 +183,18 @@ const Layout = ({ title, description, children }) => {
               ></Switch>
               <NextLink href="/cart" passHref>
                 <Link>
-                  {cart.cartItems.length > 0 ? (
-                    <Badge
-                      color="secondary"
-                      badgeContent={cart.cartItems.length}
-                    >
-                      Cart
-                    </Badge>
-                  ) : (
-                    'Cart'
-                  )}
+                  <Typography component="span">
+                    {cart.cartItems.length > 0 ? (
+                      <Badge
+                        color="secondary"
+                        badgeContent={cart.cartItems.length}
+                      >
+                        Cart
+                      </Badge>
+                    ) : (
+                      'Cart'
+                    )}
+                  </Typography>
                 </Link>
               </NextLink>
               {userInfo ? (
@@ -154,7 +240,9 @@ const Layout = ({ title, description, children }) => {
                 </>
               ) : (
                 <NextLink href="/login" passHref>
-                  <Link>Login</Link>
+                  <Link>
+                    <Typography component="span">Login</Typography>
+                  </Link>
                 </NextLink>
               )}
             </div>
